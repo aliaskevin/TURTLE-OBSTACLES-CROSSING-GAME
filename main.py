38be9b53b3e2ac893f4ab52cadd3aee70f00d2a4
Alias Kevin Kunju <alias.kevin@ymail.com>
2023-06-07 21:23:45 +0530

    Update main.py

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ from turtle import Screen
 from player import Player
 from car_manager import CarManager
 from scoreboard import Scoreboard
-
+# Initializing the player and environment
 loop_time = 0
 screen = Screen()
 screen.setup(width=600, height=600)
